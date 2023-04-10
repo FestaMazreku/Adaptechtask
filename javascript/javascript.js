b6d0dc8d615e99d9cb80717b54b29f8db6b4ed83
Festa <87881759+FestaMazreku@users.noreply.github.com>
2023-04-10 16:25:31 +0200

    update soon

---
+++ javascript/javascript.js
@@ -0,0 +1,17 @@
+$.ajax({
+    type: 'GET',
+    url: 'https://jsonplaceholder.typicode.com/posts',
+    success: function (users) {
+        array.forEach(element => {
+            
+        });
+        $id.append('');
+        $id.append('');
+        $id.append('');
+        $id.append('');
+        $id.append('');
+        $id.append('');
+        $id.append('');
+        $id.append('');
+    }
+});
