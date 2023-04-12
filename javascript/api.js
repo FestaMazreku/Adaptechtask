9cfca0c0565eab5293779c08d287066e19770d6b
Festa <87881759+FestaMazreku@users.noreply.github.com>
2023-04-12 16:28:50 +0200

    update 2

--- javascript/api.js
+++ javascript/api.js
@@ -1,32 +1,46 @@
-fetch('https://jsonplaceholder.typicode.com/posts')
-  .then(response => response.json()) // Parse JSON response
-  .then(data => {
-    const jsonDataDiv = document.getElementById('posts');
-    data.forEach(post => {
-      const postDiv = document.createElement('div');
-      postDiv.innerHTML = `<a href="#" style="font-size: 18px; font-weight: bold; color: black; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;"> Title: ${post.title}</a> 
+function getall() {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => {
+      const jsonDataDiv = document.getElementById('posts');
+      data.forEach(post => {
+        const postDiv = document.createElement('div');
+        postDiv.innerHTML = `<a href="?post=${post.id}" style="font-size: 18px; font-weight: bold; color: black; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;"> Title: ${post.title}</a> 
       <p style="color: darkblue" font-size: 25px"> Message: ${post.body}</p>`;
-      jsonDataDiv.appendChild(postDiv);
+        jsonDataDiv.appendChild(postDiv);
+        $("#comments").hide();
+      });
     });
-  });
+}
 
-// if(post.title.length < 10){
-//   post.title = post.title.slice(0, 19) + "...";
-// }
+function getPost(id) {
+  fetch('https://jsonplaceholder.typicode.com/posts/' + id)
+    .then(response => response.json())
+    .then(data => {
+      const jsonDataDiv = document.getElementById('posts');
+      const postDiv = document.createElement('div');
+      postDiv.innerHTML = `<p style="font-size: 15px; color: darkblue; font-weight: bold"> ID: ${data.id}</p> <h6> Message: ${data.body}</h6> <h6 style="color: darkgray">`;
+      jsonDataDiv.appendChild(postDiv);
+      $("#comments").show();
+    });
+
+  fetch('https://jsonplaceholder.typicode.com/comments?postId=' + id)
+    .then(response => response.json())
+    .then(data => {
+      const jsonDataDiv = document.getElementById('comments');
+      data.forEach(post => {
+        const postDiv = document.createElement('div');
+        postDiv.innerHTML = `<h6 style="color: darkblue"> Email: ${post.email} </h6> <p> Comment: ${post.body}</p>`;
+        jsonDataDiv.appendChild(postDiv);
+        $("#comments").show();
+      });
+    });
+}
+
+// $(document).ready(function () {
+//   getall();
+// });
 
 $(document).ready(function () {
-  $("#show").click(function () {
-    $("#comments").toggle();
-  });
+  getPost(5);
 });
-
-fetch('https://jsonplaceholder.typicode.com/comments?postId=1')
-  .then(response => response.json()) // Parse JSON response
-  .then(data => {
-    const jsonDataDiv = document.getElementById('comments');
-    data.forEach(post => {
-      const postDiv = document.createElement('div');
-      postDiv.innerHTML = `<p style="font-size: 15px; color: darkblue; font-weight: bold"> Email: ${post.email}</p> <h6> Name: ${post.name}</h6> <h6 style="color: darkgray"> Comment: ${post.body}</h6>`;
-      jsonDataDiv.appendChild(postDiv);
-    });
-  });
