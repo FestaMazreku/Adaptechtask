a7acccb29da5ec52fb76d01ada267decc61f0043
Festa <87881759+FestaMazreku@users.noreply.github.com>
2023-04-18 16:19:14 +0200

    update5

--- javascript/api.js
+++ javascript/api.js
@@ -21,15 +21,39 @@ function getPost(id) {
   fetch('https://jsonplaceholder.typicode.com/posts/' + id)
     .then(response => response.json())
     .then(data => {
+      $.ajax({
+        type: "GET",
+        url: 'https://jsonplaceholder.typicode.com/users',
+        dataType: 'json',
+        data: { id: data.id },
+        success: function (data) {
+          $("#user" + data.id).append(data);
+        }
+      });
+      // $.ajax({
+      //   type: "POST",
+      //   url: 'https://jsonplaceholder.typicode.com/users',
+      //   dataType: 'json',
+      //   data: {name: data.name},
+      //   success: function (data) {
+      //     $("#user" + data.name).append(data);
+      //     console.log(data.name);
+      //   }
+      // });
+      // {
+      //   //request to https://jsonplaceholder.typicode.com/users/{post.userId}
+      //   $("#user" + post.id).innerHTML(newdata)
+      // }
       const jsonDataDiv = document.getElementById('posts');
       const postDiv = document.createElement('div');
-      postDiv.innerHTML = `<p style="font-size: 15px; color: darkblue; font-weight: bold"> ID: ${data.id}</p> <h6> Message: ${data.body} </h6> 
-      <a href="file:///C:/Users/adaptech/Desktop/Adaptechtask/post.html"> <button class="btn1"> Go back </button> </a>`
+      postDiv.innerHTML = `<p style="font-size: 15px; color: darkblue; font-weight: bold"> ID: ${data.id}</p> <h6> Message: ${data.body} </h6>
+      <p id="user_${data.id}"> </p> <p style="font-weight: bold"> Name: ${data.name} </p> <h6 style="color: #0F52BA"> Email: ${data.email} </h6> <p> Username: ${data.username} </p> <p> Phone: ${data.phone} </p>
+      <a href="post.html" onclick="getPost()"> <button class="btn1"> Go back </button> </a>`
       jsonDataDiv.appendChild(postDiv);
       getComment(id);
       $("#comments").show();
     });
-}
+};
 
 function getComment(id) {
   fetch('https://jsonplaceholder.typicode.com/comments?postId=' + id)
@@ -38,7 +62,8 @@ function getComment(id) {
       const jsonDataDiv = document.getElementById('comments');
       data.forEach(post => {
         const postDiv = document.createElement('div');
-        postDiv.innerHTML = `<h6 style="font-weight: bold"> Email: ${post.email} </h6> <p style="color: darkblue; font-size: 15px"> ID: ${post.id} </p> <p style="color: darkblue"> <p> Comment: ${post.body}</p>`;
+        postDiv.innerHTML = `<h6 style="font-weight: bold"> Email: ${post.email} </h6> <p style="color: darkblue; font-size: 15px"> ID: ${post.id} </p> 
+        <p style="color: darkblue"> <p> Comment: ${post.body}</p>`;
         jsonDataDiv.appendChild(postDiv);
         $("#comments").show();
       });
@@ -51,14 +76,10 @@ function getall() {
     .then(data => {
       const jsonDataDiv = document.getElementById('posts');
       data.forEach(post => {
-        // {
-        //   //request to https://jsonplaceholder.typicode.com/users/{post.userId}
-        //   $("#user_" + post.id).innerHTML(newdata)
-        // }
         const postDiv = document.createElement('div');
-        postDiv.innerHTML = `<a href="?post=${post.id}"  style="font-size: 18px; font-weight: bold; color: black; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;"> Title: ${post.title + "..."} </a> 
-      <p style="color: darkblue" font-size: 25px"> Message: ${post.body} </p>
-      <p id="user_${post.id}"></p>`;
+        postDiv.innerHTML = `<a href="?post=${post.id}" style="font-size: 18px; font-weight: bold; color: black; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;"> 
+        Title: ${post.title + "..."} </a> 
+      <p style="color: darkblue" font-size: 25px"> Message: ${post.body} </p>`;
         jsonDataDiv.appendChild(postDiv);
         $("#comments").hide();
       });
@@ -74,5 +95,17 @@ $(document).ready(function () {
   }
   else {
     getPost(post);
+  }
+});
+
+$(document).ready(function () {
+  const currentUrl = window.location.href;
+  const searchParams = new URLSearchParams(new URL(currentUrl).search);
+  const data = searchParams.get('user');
+  if (data == null) {
+    null;
+  }
+  else {
+    getPost(data);
   }
 });
